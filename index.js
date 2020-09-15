c82ff1a1a47efd1e426043ed8f2b20a16980d275
pirmansh <fsyah1362@gmail.com>
2020-09-15 18:33:24 +0700

    add vibrate

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const show =  document.getElementById('show');
 let batas = localStorage.getItem('angka');
 let angka = 0;
 
-let tab= document.getElementById('tap').addEventListener('click', function(e){
 
+show.innerHTML = `<p id="hasil">${angka++}</p>`;
+
+let tab= document.getElementById('tap').addEventListener('click', function(e){
+    angka = 1;
     show.innerHTML = `<p id="hasil">${angka++}</p>`;
-    navigator.vibrate([50]);
+    navigator.vibrate([10]);
     if(angka == parseInt(batas)){
         navigator.vibrate([500]);
         Swal.fire({
