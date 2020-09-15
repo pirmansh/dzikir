0b498392c052d3b86c1b812bda81a58c5c656589
pirmansh <fsyah1362@gmail.com>
2020-09-15 18:29:29 +0700

    add vibrate

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ const mulai = document.getElementById('mulai').addEventListener('click', functio
           title: 'Oops...',
           text: 'Form input belum di isi!',
         });
+       
 
        } else{
         localStorage.setItem('angka', input.value);
         input.value = '';
-        window.location.replace('app.html');
+        window.location.replace('https://pirmansh.github.io/dzikir/app.html');
        }
        e.preventDefault();
 
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let angka = 0;
 let tab= document.getElementById('tap').addEventListener('click', function(e){
 
     show.innerHTML = `<p id="hasil">${angka++}</p>`;
-    navigator.vibrate([100]);
+    navigator.vibrate([50]);
     if(angka == parseInt(batas)){
         navigator.vibrate([500]);
         Swal.fire({
@@ -13,6 +13,7 @@ let tab= document.getElementById('tap').addEventListener('click', function(e){
         title: 'Alhamdulillah...',
         text: `Anda sudah berdzikir sebanyak ${batas}!`,
         });
+        window.location.replace('https://pirmansh.github.io/dzikir/index.html');
     }
     e.preventDefault();
 
